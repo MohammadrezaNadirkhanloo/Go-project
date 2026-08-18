db448c85614f23610d19d38a4f51d31e5d29bed8
kanloo <kanloo@gmail.com>
2026-08-18 08:05:02 +0330

    Feat : add jwt

--- src/go.mod
+++ src/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/didip/tollbooth/v7 v7.0.2
 	github.com/gin-gonic/gin v1.12.0
 	github.com/go-playground/validator/v10 v10.30.3
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/google/uuid v1.6.0
 	github.com/redis/go-redis/v9 v9.22.0
 	github.com/rs/zerolog v1.35.1
@@ -14,6 +15,8 @@ require (
 	github.com/swaggo/gin-swagger v1.6.1
 	github.com/swaggo/swag v1.16.6
 	go.uber.org/zap v1.28.0
+	golang.org/x/crypto v0.55.0
+	golang.org/x/time v0.15.0
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 	gorm.io/driver/postgres v1.6.2
 	gorm.io/gorm v1.31.2
@@ -75,13 +78,11 @@ require (
 	go.uber.org/multierr v1.10.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.5 // indirect
 	golang.org/x/arch v0.30.0 // indirect
-	golang.org/x/crypto v0.55.0 // indirect
 	golang.org/x/mod v0.39.0 // indirect
 	golang.org/x/net v0.57.0 // indirect
 	golang.org/x/sync v0.22.0 // indirect
 	golang.org/x/sys v0.47.0 // indirect
 	golang.org/x/text v0.41.0 // indirect
-	golang.org/x/time v0.15.0 // indirect
 	golang.org/x/tools v0.48.0 // indirect
 	google.golang.org/protobuf v1.36.12 // indirect
 )
--- src/services/user_usecase.go
+++ src/services/user_usecase.go
@@ -6,7 +6,9 @@ import (
 	"github.com/MohammadrezaNadirkhanloo/Go-project/api/dto"
 	"github.com/MohammadrezaNadirkhanloo/Go-project/common"
 	"github.com/MohammadrezaNadirkhanloo/Go-project/config"
+	"github.com/MohammadrezaNadirkhanloo/Go-project/constans"
 	"github.com/MohammadrezaNadirkhanloo/Go-project/data/db"
+	"github.com/MohammadrezaNadirkhanloo/Go-project/data/models"
 	"github.com/MohammadrezaNadirkhanloo/Go-project/pkg/logging"
 	"gorm.io/gorm"
 )
@@ -42,114 +44,30 @@ func (s *UserUsecase) SendOtp(ctx context.Context, req *dto.GetOtpRequest) error
 	return nil
 }
 
-// // Login by username
-// func (u *UserUsecase) LoginByUsername(ctx context.Context, username string, password string) (*dto.TokenDetail, error) {
-// 	user, err := u.repository.FetchUserInfo(ctx, username, password)
-
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	tokenDto := tokenDto{UserId: user.Id, FirstName: user.FirstName, LastName: user.LastName,
-// 		Email: user.Email, MobileNumber: user.MobileNumber}
-
-// 	if len(*user.UserRoles) > 0 {
-// 		for _, ur := range *user.UserRoles {
-// 			tokenDto.Roles = append(tokenDto.Roles, ur.Role.Name)
-// 		}
-// 	}
-
-// 	token, err := u.tokenUsecase.GenerateToken(tokenDto)
-
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return token, nil
-
-// }
-
-// // Register by username
-// func (u *UserUsecase) RegisterByUsername(ctx context.Context, req dto.RegisterUserByUsername) error {
-// 	user := dto.ToUserModel(req)
-
-// 	exists, err := u.repository.ExistsEmail(ctx, req.Email)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	if exists {
-// 		return &service_errors.ServiceError{EndUserMessage: service_errors.EmailExists}
-// 	}
-// 	exists, err = u.repository.ExistsUsername(ctx, req.Username)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	if exists {
-// 		return &service_errors.ServiceError{EndUserMessage: service_errors.UsernameExists}
-// 	}
-
-// 	bp := []byte(req.Password)
-// 	hp, err := bcrypt.GenerateFromPassword(bp, bcrypt.DefaultCost)
-// 	if err != nil {
-// 		u.logger.Error(logging.General, logging.HashPassword, err.Error(), nil)
-// 		return err
-// 	}
-// 	user.Password = string(hp)
-// 	_, err = u.repository.CreateUser(ctx, user)
-// 	return err
-
-// }
-
-// // Register/login by mobile number
-// func (u *UserUsecase) RegisterAndLoginByMobileNumber(ctx context.Context, mobileNumber string, otp string) (*dto.TokenDetail, error) {
-// 	err := u.otpUsecase.ValidateOtp(mobileNumber, otp)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	exists, err := u.repository.ExistsMobileNumber(ctx, mobileNumber)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	user := model.User{MobileNumber: mobileNumber, Username: mobileNumber}
-
-// 	if exists {
-// 		user, err = u.repository.FetchUserInfo(ctx, user.Username, user.Password)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-
-// 		return u.generateToken(user)
-// 	}
-
-// 	// Register and login
-// 	bp := []byte(common.GeneratePassword())
-// 	hp, err := bcrypt.GenerateFromPassword(bp, bcrypt.DefaultCost)
-// 	if err != nil {
-// 		u.logger.Error(logging.General, logging.HashPassword, err.Error(), nil)
-// 		return nil, err
-// 	}
-// 	user.Password = string(hp)
-
-// 	user, err = u.repository.CreateUser(ctx, user)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return u.generateToken(user)
-
-// }
-
-// func (u *UserUsecase) generateToken(user model.User) (*dto.TokenDetail, error) {
-// 	tokenDto := tokenDto{UserId: user.Id, FirstName: user.FirstName, LastName: user.LastName,
-// 		Email: user.Email, MobileNumber: user.MobileNumber}
-
-// 	if len(*user.UserRoles) > 0 {
-// 		for _, ur := range *user.UserRoles {
-// 			tokenDto.Roles = append(tokenDto.Roles, ur.Role.Name)
-// 		}
-// 	}
-
-// 	token, err := u.tokenUsecase.GenerateToken(tokenDto)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return token, nil
-// }
+func (s *UserUsecase) existsByEmail(email string) (bool, error) {
+	var exists bool
+	if err := s.database.Model(&models.User{}).Select("count(*)>0").Where("email=?", email).Find(&exists).Error; err != nil {
+		s.logger.Error(logging.Postgres, logging.Select, err.Error(), nil)
+	}
+	return exists, nil
+}
+func (s *UserUsecase) existsByUsername(username string) (bool, error) {
+	var exists bool
+	if err := s.database.Model(&models.User{}).Select("count(*)>0").Where("username=?", username).Find(&exists).Error; err != nil {
+		s.logger.Error(logging.Postgres, logging.Select, err.Error(), nil)
+	}
+	return exists, nil
+}
+func (s *UserUsecase) existsByMobileNumber(mobileNumber string) (bool, error) {
+	var exists bool
+	if err := s.database.Model(&models.User{}).Select("count(*)>0").Where("mobile_number=?", mobileNumber).Find(&exists).Error; err != nil {
+		s.logger.Error(logging.Postgres, logging.Select, err.Error(), nil)
+	}
+	return exists, nil
+}
+func (s *UserUsecase) getDefaultRole() (roleId int, err error) {
+	if err := s.database.Model(&models.Role{}).Select("id").Where("name=?", constans.DefaultRoleName).First(&roleId).Error; err != nil {
+		return 0, err
+	}
+	return roleId, nil
+}
