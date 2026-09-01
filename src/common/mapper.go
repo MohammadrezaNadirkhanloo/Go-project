95a3e20596bcc12032eeeb88ac2912f746bed8c2
kanloo <kanloo@gmail.com>
2026-09-01 09:05:34 +0330

    Feat : add base service

--- src/common/mapper.go
+++ src/common/mapper.go
@@ -3,15 +3,15 @@ package common
 import "encoding/json"
 
 // TODO: must write struct to struct mapper instead of this function
-func TypeConverter[T any](data any) (T, error) {
+func TypeConverter[T any](data any) (*T, error) {
 	var result T
 	dataJson, err := json.Marshal(&data)
 	if err != nil {
-		return result, err
+		return nil, err
 	}
 	err = json.Unmarshal(dataJson, &result)
 	if err != nil {
-		return result, err
+		return nil, err
 	}
-	return result, nil
+	return &result, nil
 }
--- src/services/country_services.go
+++ src/services/country_services.go
@@ -2,109 +2,43 @@ package services
 
 import (
 	"context"
-	"database/sql"
-	"time"
 
 	"github.com/MohammadrezaNadirkhanloo/Go-project/api/dto"
 	"github.com/MohammadrezaNadirkhanloo/Go-project/config"
-	"github.com/MohammadrezaNadirkhanloo/Go-project/constans"
 	"github.com/MohammadrezaNadirkhanloo/Go-project/data/db"
 	"github.com/MohammadrezaNadirkhanloo/Go-project/data/models"
 	"github.com/MohammadrezaNadirkhanloo/Go-project/pkg/logging"
-	"gorm.io/gorm"
 )
 
 type CountryService struct {
-	database *gorm.DB
-	logger   logging.Logger
+	base *BaseService[models.Country, dto.CreateAndUpdateCountryRequest, dto.CreateAndUpdateCountryRequest, dto.CountryResponse]
 }
 
 func NewCountryService(cfg *config.Config) *CountryService {
 	return &CountryService{
-		database: db.GetDB(),
-		logger:   logging.NewLogger(cfg),
+		base: &BaseService[models.Country, dto.CreateAndUpdateCountryRequest, dto.CreateAndUpdateCountryRequest, dto.CountryResponse]{
+			Database: db.GetDB(),
+			logger:   logging.NewLogger(cfg),
+		},
 	}
 }
 
-//Creat
-
+// Creat
 func (s *CountryService) Create(ctx context.Context, req *dto.CreateAndUpdateCountryRequest) (*dto.CountryResponse, error) {
-	country := models.Country{Name: req.Name}
-	country.CreatedBy = int(ctx.Value(constans.UserIdKey).(float64))
-	country.CreatedAt = time.Now().UTC()
-
-	tx := s.database.WithContext(ctx).Begin()
-
-	err := tx.Create(&country).Error
-	if err != nil {
-		tx.Rollback()
-		s.logger.Error(logging.Postgres, logging.Insert, err.Error(), nil)
-		return nil, err
-	}
-	tx.Commit()
-
-	dto := &dto.CountryResponse{Name: country.Name, Id: country.Id}
-	return dto, nil
+	return s.base.Create(ctx, req)
 }
 
-//Update
-
+// Update
 func (s *CountryService) Update(ctx context.Context, id int, req *dto.CreateAndUpdateCountryRequest) (*dto.CountryResponse, error) {
-	updateMap := map[string]interface{}{ // اون ستون هایی که قراره تغییر کنه
-		"Name":        req.Name,
-		"modified_by": &sql.NullInt64{Int64: int64(ctx.Value(constans.UserIdKey).(float64)), Valid: true},
-		"modified_at": sql.NullTime{Valid: true, Time: time.Now().UTC()},
-	}
-
-	tx := s.database.WithContext(ctx).Begin()
-
-	err := tx.Model(&models.Country{}).Where("id = ?", id).Updates(updateMap).Error
-	if err != nil {
-		tx.Rollback()
-		s.logger.Error(logging.Postgres, logging.Update, err.Error(), nil)
-		return nil, err
-	}
-	country := &models.Country{}
-	err = tx.Model(&models.Country{}).Where("id = ? AND deleted_by is null", id).First(&country).Error
-	if err != nil {
-		tx.Rollback()
-		s.logger.Error(logging.Postgres, logging.Select, err.Error(), nil)
-		return nil, err
-	}
-	tx.Commit()
-
-	dto := &dto.CountryResponse{Name: country.Name, Id: country.Id}
-	return dto, nil
+	return s.base.Update(ctx, id, req)
 }
 
 // Delete
 func (s *CountryService) Delete(ctx context.Context, id int) error {
-	deleteMap := map[string]interface{}{ // اون ستون هایی که قراره تغییر کنه
-		"deleted_by": &sql.NullInt64{Int64: int64(ctx.Value(constans.UserIdKey).(float64)), Valid: true},
-		"deleted_at": sql.NullTime{Valid: true, Time: time.Now().UTC()},
-	}
-	tx := s.database.WithContext(ctx).Begin()
-
-	err := tx.Model(&models.Country{}).Where("id = ?", id).Updates(deleteMap).Error
-	if err != nil {
-		tx.Rollback()
-		s.logger.Error(logging.Postgres, logging.Delete, err.Error(), nil)
-		return err
-	}
-	tx.Commit()
-	return nil
+	return s.base.Delete(ctx, id)
 }
 
 // Get by Id
 func (s *CountryService) GetById(ctx context.Context, id int) (*dto.CountryResponse, error) {
-	country := &models.Country{}
-
-	err := s.database.Where("id = ?", id).First(&country).Error
-	if err != nil {
-		s.logger.Error(logging.Postgres, logging.Select, err.Error(), nil)
-		return nil, err
-	}
-
-	dto := &dto.CountryResponse{Name: country.Name, Id: country.Id}
-	return dto, nil
+	return s.base.GetById(ctx, id)
 }
